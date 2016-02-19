a5ce2e39c48dc5247c8f70a105722cca7ef54fcb
Younes SADMI <younes.sadmi@gmail.com>
2016-02-20 00:27:25 +0100

    ajout des views pour ArticleController

--- module/Article/src/Article/Controller/ArticleController.php
+++ module/Article/src/Article/Controller/ArticleController.php
@@ -11,43 +11,19 @@ class ArticleController extends AbstractActionController
     {
     }
 
-    public function loginAction()
+    public function ajax_comment_ratingAction()
     {
     }
 
-    public function logoutAction()
+    public function afficherAction()
     {
     }
 
-    public function gestion_pageAction()
+    public function categorieAction()
     {
     }
 
-    public function gestion_articleAction()
-    {
-    }
-
-    public function gestion_newsletterAction()
-    {
-    }
-
-    public function gestion_commentAction()
-    {
-    }
-
-    public function gestion_categorieAction()
-    {
-    }
-
-    public function gestion_adminAction()
-    {
-    }
-
-    public function kpiAction()
-    {
-    }
-
-    public function gestion_profilAction()
+    public function ajax_ratingAction()
     {
     }
 }
