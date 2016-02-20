55f3c36715a413533f93cec8e98e948a01b68fe2
Younes SADMI <younes.sadmi@gmail.com>
2016-02-20 02:12:07 +0100

    Article model created

--- module/Article/src/Article/Controller/ArticleController.php
+++ module/Article/src/Article/Controller/ArticleController.php
@@ -7,8 +7,13 @@ use Zend\View\Model\ViewModel;
 
 class ArticleController extends AbstractActionController
 {
+    protected $articleTable;
+
     public function indexAction()
     {
+        return new ViewModel([
+            'articles' => $this->getArticleTable()->fetchAll(),
+        ]);
     }
 
     public function ajax_comment_ratingAction()
@@ -25,5 +30,15 @@ class ArticleController extends AbstractActionController
 
     public function ajax_ratingAction()
     {
+    }
+
+    //the ServiceManager can create an AlbumTable instance for us, we can add a method to the controller to retrieve it.
+    public function getArticleTable()
+    {
+        if (!$this->articleTable) {
+            $sm = $this->getServiceLocator();
+            $this->articleTable = $sm->get('Article\Model\ArticleTable');
+        }
+        return $this->articleTable;
     }
 }
